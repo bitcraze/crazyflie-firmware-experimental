d066c43184a1a4a2b131cb5d989cc956752cbbcb
Tobias Antonsson <tobias@bitcraze.se>
2017-01-03 13:48:07 +0100

    Glow: Added strobe functionality.

--- src/deck/drivers/src/glow.c
+++ src/deck/drivers/src/glow.c
@@ -37,15 +37,25 @@
 #include "log.h"
 #include "param.h"
 
+#define GLOW_TIM                   TIM3
+#define GLOW_TIM_CLOCK_HZ_DIV2     84000000UL
+#define GLOW_TIM_PRESCALER         0xFFFF
+#define GLOW_TIM_STROBE_CLK_FREQ  (GLOW_TIM_CLOCK_HZ_DIV2 / GLOW_TIM_PRESCALER)
+
+
+#define GLOW_STROBE_ON_TIME_CNT 20
 #define GLOW_UPDATE_PERIOD_MS   10
-#define GLOW_PWM_FREQ           20000
-#define GLOW_TIM_PERIOD
+
 #define GLOW_LED_MAX_AMP        0.50f
-#define GLOW_AMP_AT_3p7_VOLT    1.35f
+#define GLOW_AMP_AT_3p0_VOLT    0.35f
+#define GLOW_AMP_AT_3p7_VOLT    1.18f
 #define GLOW_AMP_AT_4p2_VOLT    1.80f
 
-
+typedef enum {PWM, STROBE } GlowState;
+GlowState glowState;
 static uint8_t power;
+static uint8_t freqDeciHz;
+static uint8_t prevFreqCentiHz;
 static uint16_t duty;
 
 static void glowOn(uint8_t newPower)
@@ -55,8 +65,31 @@ static void glowOn(uint8_t newPower)
 
 static void glowOff()
 {
-  TIM_SetCompare2(TIM3, 0);
+  TIM_SetCompare2(GLOW_TIM, 0);
   power = 0;
+}
+
+static void glowUpdateTimerConfig(uint16_t duty, uint8_t frequencyDeciHz)
+{
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+
+  // Update compare before timer change
+  TIM_SetCompare2(GLOW_TIM, duty);
+
+  if (frequencyDeciHz)
+  {
+    TIM_TimeBaseStructure.TIM_Prescaler = (GLOW_TIM_PRESCALER - 1);
+    TIM_TimeBaseStructure.TIM_Period = (uint16_t)((GLOW_TIM_STROBE_CLK_FREQ * 10) / frequencyDeciHz);
+    TIM_TimeBaseInit(GLOW_TIM, &TIM_TimeBaseStructure);
+  }
+  else
+  {
+    TIM_TimeBaseStructure.TIM_Period = 0xFFFF;
+    TIM_TimeBaseStructure.TIM_Prescaler = 0;
+    TIM_TimeBaseInit(GLOW_TIM, &TIM_TimeBaseStructure);
+  }
 }
 
 void glowTask(void* parameters)
@@ -76,7 +109,23 @@ void glowTask(void* parameters)
     maxDuty = ((GLOW_LED_MAX_AMP / GLOW_AMP_AT_3p7_VOLT) * 0xFFFF) * batComp;
     duty = ((uint32_t)power * maxDuty) / 256;
 
-    TIM_SetCompare2(TIM3, duty);
+    // Update timer settings and duty
+    if ((glowState == PWM && freqDeciHz != 0) ||
+        (freqDeciHz != prevFreqCentiHz))
+    {
+      glowUpdateTimerConfig(GLOW_STROBE_ON_TIME_CNT, freqDeciHz);
+      glowState = STROBE;
+      prevFreqCentiHz = freqDeciHz;
+    }
+    else if (glowState == STROBE && freqDeciHz == 0)
+    {
+      glowUpdateTimerConfig(duty, freqDeciHz);
+      glowState = PWM;
+    }
+    else if (glowState == PWM)
+    {
+      TIM_SetCompare2(GLOW_TIM, duty);
+    }
 
     vTaskDelayUntil(&xLastWakeTime, M2T(GLOW_UPDATE_PERIOD_MS));
   }
@@ -91,7 +140,7 @@ static void glowInit(DeckInfo *info)
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-  /* GPIOB Configuration: TIM3 Channel 2 as alternate function push-pull */
+  /* GPIOB Configuration: GLOW_TIM Channel 2 as alternate function push-pull */
   // Configure the GPIO PB5 for the timer output
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -109,24 +158,25 @@ static void glowInit(DeckInfo *info)
   TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
   TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
-  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(GLOW_TIM, &TIM_TimeBaseStructure);
 
   /* PWM1 Mode configuration: Channel2 */
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = 0;
   TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+  TIM_OC2Init(GLOW_TIM, &TIM_OCInitStructure);
 
-  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
-  TIM_CtrlPWMOutputs(TIM3, ENABLE);
+  TIM_OC2PreloadConfig(GLOW_TIM, TIM_OCPreload_Enable);
+  TIM_CtrlPWMOutputs(GLOW_TIM, ENABLE);
 
   DBGMCU_APB1PeriphConfig(DBGMCU_TIM3_STOP, ENABLE);
 
-  TIM_Cmd(TIM3, ENABLE);
+  TIM_Cmd(GLOW_TIM, ENABLE);
 
   xTaskCreate(glowTask, "glow", configMINIMAL_STACK_SIZE, NULL, 1/*priority*/, NULL);
 
+  glowState = PWM;
   glowOff();
   glowOn(20);
 
@@ -151,5 +201,6 @@ LOG_GROUP_STOP(glow)
 
 PARAM_GROUP_START(glow)
 PARAM_ADD(PARAM_UINT8, power, &power)
+PARAM_ADD(PARAM_UINT8, stobeFreqDeciHz, &freqDeciHz)
 PARAM_GROUP_STOP(glow)
 
