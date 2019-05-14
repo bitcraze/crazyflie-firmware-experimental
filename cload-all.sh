45ffad3f3f6ea26565edbb70cb2e7ef81292841a
Arnaud Taffanel <arnaud@bitcraze.io>
2019-05-14 09:10:55 +0200

    Add CF7 and 8 to cload_all.sh

--- cload-all.sh
+++ cload-all.sh
@@ -6,3 +6,6 @@ python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E703
 python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E704
 python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E705
 python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E706
+python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E707
+python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E708
+
