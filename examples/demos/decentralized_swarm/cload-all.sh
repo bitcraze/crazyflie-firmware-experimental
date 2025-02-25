3bf05243bc2a2fae49b6e6df66600c335c40f152
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2025-02-25 13:14:56 +0100

    Update the script to flash all demo Crazyflies

--- examples/demos/decentralized_swarm/cload-all.sh
+++ examples/demos/decentralized_swarm/cload-all.sh
@@ -5,14 +5,10 @@ YELLOW='\033[0;33m'         # Yellow
 RED='\033[0;31m'            # Red
 
 # Bash ranges can be continious like in {1..9} or non continious like in 2 {4..5}
-for i in {1..6}; do
+# Change the defconfig to match the platform
+for i in {1..9}; do
+    make cf21bl_defconfig
     make -j 8
-    printf "${YELLOW}Flashing ${RED}CF${YELLOW} 0${i}${COLOR_RESET}\n"
-    CLOAD_CMDS="-w radio://0/90/2M/E7E7E7E70${i}" make cload
-done
-
-for i in {7..9}; do
-    make -j 8 BRUSHLESS=1
     printf "${YELLOW}Flashing ${RED}CF BRUSHLESS${YELLOW} 0${i}${COLOR_RESET}\n"
-    CLOAD_CMDS="-w radio://0/90/2M/E7E7E7E70${i}" make cload
+    CLOAD_CMDS="-w radio://0/80/2M/ABAD1DEA0${i}" make cload
 done
