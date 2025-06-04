5b8493e76e724124d7dd183a3df26ce604789d47
aris <arismorsinkpaloumpas@gmail.com>
2025-06-04 12:01:50 +0200

    Updated write-geo-all.py file and added the new configuration

--- examples/demos/decentralized_swarm/write-geo-all.py
+++ examples/demos/decentralized_swarm/write-geo-all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
 import logging
 import sys
+import time
 
 import cflib.crtp  # noqa
 from cflib.crazyflie import Crazyflie
@@ -10,34 +10,27 @@ from cflib.localization import LighthouseConfigWriter
 
 
 uris = [
-    'radio://0/90/2M/E7E7E7E701',
-    'radio://0/90/2M/E7E7E7E702',
-    'radio://0/90/2M/E7E7E7E703',
-    'radio://0/90/2M/E7E7E7E704',
-    'radio://0/90/2M/E7E7E7E705',
-    'radio://0/90/2M/E7E7E7E706',
-    'radio://0/90/2M/E7E7E7E707',
-    'radio://0/90/2M/E7E7E7E708',
-    'radio://0/90/2M/E7E7E7E709',
+    'radio://0/80/2M/ABAD1DEA01',
+    'radio://0/80/2M/ABAD1DEA02',
+    'radio://0/80/2M/ABAD1DEA03',
+    'radio://0/80/2M/ABAD1DEA04',
+    'radio://0/80/2M/ABAD1DEA05',
+    'radio://0/80/2M/ABAD1DEA06',
+    'radio://0/80/2M/ABAD1DEA07',
+    'radio://0/80/2M/ABAD1DEA08',
 ]
-
-
-def data_stored_cb(success):
-    if success:
-        print("Geometry uploaded")
-    else:
-        print("WARNING: Failed to upload geometry!")
 
 
 def write_one(file_name: str, uri: str):
     print(f'Writing to {uri}')
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         writer = LighthouseConfigWriter(scf.cf)
-        writer.write_and_store_config_from_file(data_stored_cb, file_name)
+        writer.write_and_store_config_from_file(None, file_name)
+        time.sleep(1)
 
 
 if len(sys.argv) < 2:
-    raise ValueError("File name missing")
+    raise ValueError('File name missing')
 file_name = sys.argv[1]
 
 print(f"Using file {file_name}")
